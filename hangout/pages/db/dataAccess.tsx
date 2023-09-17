9b462f9d269706bf5551d3a61c826706d58686be
RosenlindDylan <35314066+RosenlindDylan@users.noreply.github.com>
2023-09-16 19:47:37 -0700

    seperate query file

---
+++ hangout/pages/db/dataAccess.tsx
@@ -0,0 +1,26 @@
+
+const connectToDB = require('./db');
+
+export async function insertData(data) {
+    const db = await connectToDB();
+    const collection = db.collection('hangout');
+    
+    try {
+        const result = await collection.insertOne(data);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } catch (e) {
+        console.error(e);
+    }  
+}
+
+export async function getData(query) {
+    const db = await connectToDB();
+    const collection = db.collection('hangout'); // is connecting everytime valid or na?
+
+    try {
+        const result = await collection.findOne({}, query);
+        console.log(`A document was retrieved`)
+    } catch (e) {
+        console.error(e);
+    }
+}
